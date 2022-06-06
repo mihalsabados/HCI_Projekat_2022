184826874bc350809d7b431b762fad5324ba8772
zoricavukovic <zoka200015@gmail.com>
2022-06-06 11:06:55 +0200

    Odradjen prikaz mesecnih prihoda za izabranu godinu.

---
+++ HCI_Projekat/database/CardRepository.cs
@@ -0,0 +1,88 @@
+﻿using HCI_Projekat.model;
+using HCI_Projekat.services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCI_Projekat.database
+{
+    public static class CardRepository
+    {
+        private static List<Card> cards = new List<Card>()
+        {
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2021, 6, 13, 4, 48, 0), 1, 1, 400),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2021, 5, 13, 4, 48, 0), 1, 1, 350),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2021, 5, 12, 4, 48, 0), 1, 1, 429),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 12, 13, 4, 48, 0), 1, 1, 700),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 4, 13, 4, 48, 0), 1, 1, 657),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 1, 13, 4, 48, 0), 1, 1, 657),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 2, 13, 4, 48, 0), 1, 1, 657),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 3, 13, 4, 48, 0), 1, 1, 657),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 4, 11, 4, 48, 0), 1, 1, 657),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 5, 13, 4, 48, 0), 1, 1, 657),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 6, 7, 4, 48, 0), 1, 1, 167),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 7, 13, 4, 48, 0), 1, 1, 1200),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 8, 4, 4, 48, 0), 1, 1, 899),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 9, 16, 4, 48, 0), 1, 1, 324),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 10, 13, 4, 48, 0), 1, 1, 440),
+            new Card(RouteService.FindRouteByName("1"), new DateTime(2022, 11, 11, 4, 48, 0), 1, 1, 657),
+        };
+
+        internal static List<double> RevenueForMonths(int yearRevenue)
+        {
+            List<double> revenue = new List<double>();
+            Dictionary<int, double> dicRev = new Dictionary<int, double>();
+            InitializationDict(dicRev);
+            foreach (Card card in cards)
+            {
+                if (card.DateTimeForCard.Year == yearRevenue)
+                {
+                    if (dicRev.ContainsKey(card.DateTimeForCard.Month))
+                    {
+                        dicRev[card.DateTimeForCard.Month] += card.Price;
+                    }
+                    else
+                        dicRev.Add(card.DateTimeForCard.Month, card.Price);
+                    
+                }
+            }
+            foreach (double a in dicRev.Values)
+            {
+                revenue.Add(a);
+            }
+            return revenue;
+        }
+
+        private static void InitializationDict(Dictionary<int, double> dicRev)
+        {
+            dicRev.Add(1, 0);
+            dicRev.Add(2, 0);
+            dicRev.Add(3, 0);
+            dicRev.Add(4, 0);
+            dicRev.Add(5, 0);
+            dicRev.Add(6, 0);
+            dicRev.Add(7, 0);
+            dicRev.Add(8, 0);
+            dicRev.Add(9, 0);
+            dicRev.Add(10, 0);
+            dicRev.Add(11, 0);
+            dicRev.Add(12, 0);
+        }
+
+        internal static List<int> GetAvailableYearsWithSoldCards()
+        {
+            List<int> years = new List<int>();
+            foreach (Card card in cards)
+            {
+                if (!years.Contains(card.DateTimeForCard.Year))
+                    years.Add(card.DateTimeForCard.Year);
+            }
+            return years;
+        }
+
+        public static List<Card> GetCards()
+        {
+            return cards;
+        }
+    }
+}
---
+++ HCI_Projekat/gui/SoldCardsPerMonth.xaml.cs
@@ -0,0 +1,145 @@
+﻿using HCI_Projekat.services;
+using LiveCharts;
+using LiveCharts.Configurations;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HCI_Projekat.gui
+{
+    /// <summary>
+    /// Interaction logic for SoldCardsPerMonth.xaml
+    /// </summary>
+    public partial class SoldCardsPerMonth : Page
+    {
+        public SoldCardsPerMonth()
+        {
+            InitializeComponent();
+            AddYears();
+            loadBarChart();
+        }
+
+        private void AddYears()
+        {
+            List<int> years = CardService.GetAvailableYearsWithSoldCards();
+            if (years.Count == 0)
+                searchedYears.Visibility = Visibility.Visible;
+            else
+            {
+                searchedYears.Visibility = Visibility.Hidden;
+                foreach (int year in years)
+                    yearsComboBox.Items.Add(createComboBoxItem(year));
+                int maxYear = years.Max();
+                int i = 0;
+                foreach (int a in years)
+                {
+                    if (maxYear == a)
+                        break;
+                    i++;
+                }
+                yearsComboBox.SelectedIndex = i;
+            }
+             
+        }
+
+        private ComboBoxItem createComboBoxItem(int content)
+        {
+            ComboBoxItem c = new ComboBoxItem();
+            c.Content = content + "";
+            c.Visibility = Visibility.Visible;
+            c.Foreground = (Brush)(new BrushConverter().ConvertFrom("#FF485B83"));
+           
+            return c;
+        }
+
+        private void loadBarChart()
+        {
+            columnChart.Series.Clear();
+            columnChart.AxisX.Clear();
+            columnChart.AxisY.Clear();
+
+            ComboBoxItem item = (ComboBoxItem)yearsComboBox.SelectedItem;
+            int yearRevenue = Int32.Parse(item.Content.ToString());
+            List<double> revenue = CardService.RevenueForMonths(yearRevenue);
+
+            label1.Content = "Mesečni prihodi od prodatih karata u " + yearRevenue + ".";
+            label1.HorizontalAlignment = HorizontalAlignment.Center;
+
+            var chartValues = new ChartValues<double>();
+            foreach (var monthly in revenue)
+            {
+                
+                chartValues.Add(monthly);
+            }
+
+
+            var seriesCollection = new SeriesCollection
+            {
+               new ColumnSeries
+                {
+                    Title = "Prihodi po mesecu",
+                    Values = chartValues,
+                    Configuration = new CartesianMapper<double>()
+                    .Y(point => point)
+                    .Stroke(point => (point == chartValues.Max()) ? (Brush)(new BrushConverter().ConvertFrom("#FFECA77D")) : (point == chartValues.Min()) ? (Brush)(new BrushConverter().ConvertFrom("#FFC34E43")) : (Brush)(new BrushConverter().ConvertFrom("#FF485B83")))
+                    .Fill(point => (point == chartValues.Max()) ?  (Brush)(new BrushConverter().ConvertFrom("#FFECA77D")) : (point == chartValues.Min()) ? (Brush)(new BrushConverter().ConvertFrom("#FFC34E43")) : (Brush)(new BrushConverter().ConvertFrom("#FF485B83"))),
+               }
+            };
+
+
+            seriesCollection.Add(new LineSeries()
+            {
+                Title = "Prihodi po mesecu",
+                Values = chartValues,
+                DataLabels = false,
+                Fill = Brushes.Transparent,
+                PointGeometrySize = 0,
+                IsEnabled = false,
+                Configuration = new CartesianMapper<double>()
+                   .Y(point => point)
+            });
+
+
+            columnChart.AxisX.Add(new Axis
+            {
+                Title = "Mesec",
+                Labels = new[] { "Jan", "Feb", "Mart", "April", "Maj", "Jun", "Jul", "Avg", "Sept", "Okt", "Nov", "Dec" },
+                Foreground = (Brush)(new BrushConverter().ConvertFrom("#FF485B83")),
+                FontSize = 14,
+                Separator = new LiveCharts.Wpf.Separator { Step = 1 }
+
+            }) ;
+
+            Func<double, string> labFormat = value => value.ToString("C");
+           
+            columnChart.AxisY.Add(new Axis
+            {
+                Title = "Prihodi (RSD)",
+                LabelFormatter = labFormat,
+                Foreground = (Brush)new BrushConverter().ConvertFrom("#FF485B83"),
+                FontSize = 14
+
+            });
+
+            columnChart.Series = seriesCollection;
+
+        }
+
+        private void yearsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            loadBarChart();
+        }
+    }
+}
